71019315eba97573eddf8933b0ae115db67da6ca
imachumphries <imachumphries@me.com>
2015-08-14 20:28:36 -0400

    Added background shader and GLSL waterShader program

--- Mar/Background.m
+++ Mar/Background.m
@@ -13,14 +13,41 @@
 
 - (id)initWithImageNamed:(NSString *)name {
     if (self = [super initWithImageNamed:name]) {
+        [self setLightingBitMask:1];
         [self setNormalTexture:[SKTexture textureWithImage:[UIImage imageNamed:name]]];
+        
         [self setSize:CGSizeMake(WIDTH, HEIGHT)];
         [self setPosition:CGPointMake(WIDTH/2, HEIGHT/2)];
-        [self setLightingBitMask:1];
         [self setZPosition:0];
+        
+        [self addWaterShader];
     
     }
     return self;
+}
+
+- (void)addWaterShader {
+   
+    shader = [SKShader shaderWithFileNamed:@"waterShader.fsh"];
+    [self loadUniforms];
+    self.shader = shader;
+
+}
+
+- (void)loadUniforms {
+    shader.uniforms = @[
+                        [SKUniform uniformWithName:@"u_dudvMap" texture:[SKTexture textureWithImage:[UIImage imageNamed:@"waterDUDV.png"]]],
+                        [SKUniform uniformWithName:@"u_myTexture" texture:self.texture],
+                        [SKUniform uniformWithName:@"u_moveFactor" float:moveFactor],
+                        ];
+}
+
+
+- (void)update:(NSTimeInterval)currentTime {
+    float WAVE_SPEED = .00000001f;
+    moveFactor += WAVE_SPEED * currentTime;
+    moveFactor = fmodf(moveFactor, 1.0f);
+   [self loadUniforms];
 }
 
 @end
---
+++ Mar/waterShader.fsh
@@ -0,0 +1,19 @@
+void main( void )
+{
+
+    highp float waveStrength = 0.004;
+    vec4 val = texture2D(u_myTexture, v_tex_coord);
+
+    vec2 distortion1 = (texture2D(u_dudvMap, vec2(v_tex_coord.x + u_moveFactor, v_tex_coord.y)).rg * 1.9 - 1.0) *waveStrength;
+    vec2 distortion2 = (texture2D(u_dudvMap, vec2(-v_tex_coord.x + u_moveFactor, v_tex_coord.y + u_moveFactor)).rg * 1.9 - 1.0) *waveStrength;
+    vec2 totalDistortion = distortion1 + distortion2;
+    
+    
+    vec2 texCoords = v_tex_coord;
+    texCoords += totalDistortion;
+    texCoords = clamp(texCoords, 0.001, 0.999);
+    
+   vec4 newColor = texture2D(u_myTexture, texCoords);
+    
+    gl_FragColor = newColor;
+}
